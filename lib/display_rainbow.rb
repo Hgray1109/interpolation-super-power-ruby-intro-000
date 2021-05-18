e626ed0bf0f65c9b24adbfdd4ba43335fba5213f
Hashem Gray <hgray1109@gmail.com>
2021-05-18 01:44:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
 index = 0
- puts "R: #{colors[index + 1]}, O: #{colors[index + 1]}, Y: #{colors[index + 1]}, G: #{colors[index + 1]}, B: #{colors[index + 1]}, I: #{colors[index + 1]}, V: #{colors[index + 1]}"
+ puts "R: #{colors[index++]}, O: #{colors[index + 1]}, Y: #{colors[index + 1]}, G: #{colors[index + 1]}, B: #{colors[index + 1]}, I: #{colors[index + 1]}, V: #{colors[index + 1]}"
 end
